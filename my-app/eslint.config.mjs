2c1233de41eb266ee6f91ac0b04fc4a8af8e6587
slimmsyd <ssanderss444@gmail.com>
2025-03-03 19:15:44 -0500

    update

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -16,7 +16,9 @@ const eslintConfig = [
       'react/no-unescaped-entities': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off', // Disable the rule for explicit any types
-
+      'react-hooks/exhaustive-deps': 'warn', // Change from 'error' to 'warn' for useEffect dependencies
+      '@next/next/no-img-element': 'off', // Disable the warning for <img> elements
+      'jsx-a11y/alt-text': 'warn', // Change from 'error' to 'warn' for alt text requirements
     },
   },
 ];
